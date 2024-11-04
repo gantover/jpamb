6adb01cedfa8dd9b4d70c2ff17ee67f83772a026
Zdeněk Pazdera <zden.pazdera@gmail.com>
2024-11-04 16:18:22 +0100

    feat: always recursive and always looping recursion tests

--- src/main/java/jpamb/cases/Recursions.java
+++ src/main/java/jpamb/cases/Recursions.java
@@ -1,16 +1,41 @@
 package jpamb.cases;
 
+import jpamb.utils.Case;
+
 public class Recursions {
-    public static int factorial(int n) {
-        if (n > 1) {
-            return n * factorial(n - 1);
-        }
-        return 1;
+
+  @Case("(100000) -> depth exceeded")
+  @Case("(5) -> ok")
+  public static int factorial(int n) {
+    if (n > 1) {
+      return n * factorial(n - 1);
     }
-    public static int badFactorial(int n) {
-        if (n > 1) {
-            return n * badFactorial(n + 1);
-        }
-        return 1;
+    return 1;
+  }
+
+  @Case("(5) -> depth exceeded")
+  @Case("(1) -> ok")
+  public static int badFactorial(int n) {
+    if (n > 1) {
+      return n * badFactorial(n + 1);
     }
+    return 1;
+  }
+
+  @Case("() -> depth exceeded")
+  public static void alwaysRecursive() {
+    alwaysRecursive();
+  }
+
+  @Case("(1) -> *")
+  public static int neverFactorial(int n) {
+    int i = 1;
+    while (i > 0) {
+    }
+
+    if (n > 1) {
+      return n * neverFactorial(n - 1);
+    }
+    return 1;
+  }
 }
